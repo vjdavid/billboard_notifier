ec5a49c11821e862e1cd3c2d806792a5e5195de5
vjdavid <vjdavid621@gmail.com>
2016-03-16 12:48:31 -0700

    removed method that no are longer using why have same feautre of new method movie_list

--- billboard.rb
+++ billboard.rb
@@ -12,19 +12,6 @@ class Billboard
     JSON.parse(Net::HTTP.get(URI(uri))[16...-1])
   end
 
-  def send_email
-    mail = Mail.new do
-      from    'example@example.com'
-      to      'example@example.com'
-      subject 'Currently your movie is on presale'
-      body    'Whooraay gohead and enjoy!'
-    end
-
-    parsed_body.each do |title|
-      mail.deliver! if title["Titulo"].match(/batman/i) || title['Titulo'].match(/superman/i)
-    end
-  end
-
   def movie_list(movie)
     mail = Mail.new do
       from    'example@example.com'
