f5fcb7821ad2135e96784a2b194d8ef075969dfa
vjdavid <vjdavid621@gmail.com>
2016-03-16 14:35:30 -0700

    added syntax of ruby 1.9

--- email_config.rb
+++ email_config.rb
@@ -1,10 +1,9 @@
 require 'mail'
 
 Mail.defaults do
-  delivery_method :smtp, {
-    :port      => 587,
-    :address   => "smtp.mandrillapp.com",
-    :user_name => "YOUR_USERNAME",
-    :password  => "VALID_API_KEY"
-  }
+  delivery_method :smtp,
+    port:       587,
+    address:    'smtp.mandrillapp.com',
+    user_name:  ENV['YOUR_USERNAME'],
+    password:   ENV['VALID_API_KEY']
 end
