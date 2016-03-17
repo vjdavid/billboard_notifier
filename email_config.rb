db69404c001354f72c43aa6791d4d1686d6bbfe8
vjdavid <vjdavid621@gmail.com>
2016-03-16 17:55:56 -0700

    added EmailConfig class to setup variables of the email as well added a new method called send_email for use on the child class

--- email_config.rb
+++ email_config.rb
@@ -1,9 +1,26 @@
 require 'mail'
 
-Mail.defaults do
-  delivery_method :smtp,
-    port:       587,
-    address:    'smtp.mandrillapp.com',
-    user_name:  ENV['YOUR_USERNAME'],
-    password:   ENV['VALID_API_KEY']
+class EmailConfig
+
+  def initialize
+    Mail.defaults do
+      delivery_method :smtp,
+        port:       587,
+        address:    'smtp.mandrillapp.com',
+        user_name:  ENV['YOUR_USERNAME'],
+        password:   ENV['VALID_API_KEY']
+    end
+  end
+
+  def send_email
+    mail = Mail.new do
+      from    ENV['FROM_EMAIL']
+      to      ENV['TO_EMAIL']
+      subject 'Currently your movie is on presale'
+      body    'Whooraay gohead and enjoy!'
+    end
+
+    mail.deliver!
+  end
+
 end
